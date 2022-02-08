754842bc4f8a773a484c70f06dd9ac849e2a3699
tondapu uma <tondapuma03@gmail,com>
2022-02-08 16:13:45 +0530

    added new feature

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def my_fun(a,b):
+    return a+b
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Uma')
     print("How are you")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(my_fun(2,3))
